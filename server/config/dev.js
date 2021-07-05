8dab146a93c49cad10b3a7229ee6b2c3840bb73d
kutarobo <kutarobo@naver.com>
2021-07-06 04:15:26 +0900

    feat: Add config
    config 설정

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://doosik:dPwpqlqjs1234@movie-site.07uxx.mongodb.net/youtube?retryWrites=true&w=majority",
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? 2 : 0,
+  },
+};
