71d03cb8533869a9c244cdecf52e0f0b77b64588
kutarobo <kutarobo@naver.com>
2021-07-11 08:01:20 +0900

    feat: Add video list

--- server/routes/video.js
+++ server/routes/video.js
@@ -50,6 +50,17 @@ router.post("/uploadVideo", (req, res) => {
   });
 });
 
+router.get("/getVideos", (req, res) => {
+  Video.find()
+    .populate("writer")
+    .exec((err, videos) => {
+      if (err) {
+        return res.status(400).send(err);
+      }
+      res.status(200).json({ success: true, videos });
+    });
+});
+
 router.post("/thumbnail", (req, res) => {
   // todo 썸네일 생성하고 비디오 러닝타임 가져오기
 
